d2928a548310cc1b73545e5d7c164d034ab06b2c
Caetano Santos <caetano.s.s@hotmail.com>
2020-06-23 10:07:35 -0300

    Criar Background

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let convas = document.getElementById("snake");
+let context = convas.getContext("2d");
+let box = 32;
+
+function criarBG(){
+    context.fillStyle ="lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+criarBG();
